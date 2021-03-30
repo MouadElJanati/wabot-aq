effe72c24631f1beac42d980712e1311233c39af
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 09:08:11 +0100

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -7,9 +7,9 @@ handler.before = m => {
 	let before = user.level * 1
 	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
 	if (before !== user.level) m.reply(`
-Selamat, anda telah naik level!
+Congratulations, you have leveled up!
 *${before}* -> *${user.level}*
-gunakan *.profile* untuk mengecek
+use *.profile* to check
 	`.trim())
 }
 
