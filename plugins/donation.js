66bbe4a56195db0de8ec5e6dc418af5edb413d83
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 09:56:23 +0100

    Update and rename donasi.js to donation.js

--- plugins/donation.js
+++ plugins/donation.js
@@ -3,9 +3,9 @@ let handler = async m => m.reply(`
 │ • Indosat Ooredoo [081515860089]
 │ • Telkomsel [081334177340]
 ╰────
-`.trim()) // Tambah sendiri kalo mau
-handler.help = ['donasi']
+`.trim()) // add yourself if you want
+handler.help = ['donation']
 handler.tags = ['info']
-handler.command = /^dona(te|si)$/i
+handler.command = /^dona(te|tion)$/i
 
 module.exports = handler
