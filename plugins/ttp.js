5862d9d9f7fb58f3ee68bee424f09ebe78fb5a1e
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 13:08:45 +0100

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -2,13 +2,13 @@ const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Tidak ada teks?'
+  if (!text) throw 'No text?'
   let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text }), global.packname, global.author)
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
 }
-handler.help = ['ttp <teks>']
+handler.help = ['ttp <text>']
 handler.tags = ['sticker']
 handler.command = /^ttp$/i
 handler.owner = false
