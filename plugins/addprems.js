994e349ef3368130259bc2b6ae42e67cdf35e754
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 03:50:38 +0100

    Update addprems.js

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -4,14 +4,14 @@ let handler = async(m, { conn, text }) => {
 let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw 'Tag salah satu lah,dan masukkan nomor untuk di verivikasi !'
+  if (!who) throw 'Tag one of them, and enter the number for verification !'
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].premium = true
-  var nomor = m.sender
-    m.reply(`*Done berhasil added User✅*\n\n*Nomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Expired:* 30Days\n*Thanks For Added Premium !*`)
+  var number = m.sender
+    m.reply(`*Done successfully add User✅*\n\n*Number : wa.me/${number.split("@s.whatsapp.net")[0]}\n*Expired:* 30Days\n*Thanks For Added Premium !*`)
   // } else m.reply('Ada nomor host disini...')
 }
-handler.help = ['addprems <nomor>']
+handler.help = ['addprems <number>']
 handler.tags = ['owner']
 handler.command = /^addprems$/i
 handler.rowner = true
