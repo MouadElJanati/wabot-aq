8b85197f01eec8e1063754a3a61da13342adeda5
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 12:21:24 +0100

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -1,15 +1,15 @@
 // By RC047 :V
 
 let handler = async(m, { conn, text }) => {
-    if (!text) return conn.reply(m.chat, 'Silahkan masukkan laporan', m)
-    if (text > 300) return conn.reply(m.chat, 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks!', m)
+    if (!text) return conn.reply(m.chat, 'Please enter a report', m)
+    if (text > 300) return conn.reply(m.chat, 'Sorry Text Too Long, Maximum 300 Text!', m)
     const laporan = `*「 REPORT 」*\nNomor : wa.me/${m.sender.split`@`[0]}\nPesan : ${text}`
     for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid))
     m.reply(laporan, jid)
     m.reply(laporan, m.sender) // Mwehehehehe
-    conn.reply(m.chat, '✔️Masalah telah di laporkan ke Owner Bot, laporan palsu/main2 tidak akan ditanggapi!', m)
+    conn.reply(m.chat, '✔️Problems have been reported to Owner Bot, fake/main2 reports will not be responded to!', m)
 }
-handler.help = ['bug', 'report'].map(v => v + ' <laporan>')
+handler.help = ['bug', 'report'].map(v => v + ' <report>')
 handler.tags = ['info']
 handler.command = /^(bug|report)$/i
 handler.owner = false
