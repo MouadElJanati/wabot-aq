723d59ac5da83f1a909f546ccd2702938a681ca2
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 14:53:37 +0100

    Add files via upload

---
+++ plugins/nsiit.js
@@ -0,0 +1,39 @@
+let handler = async (m, { conn }) => {
+
+  if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
+
+    global.DATABASE._data.users[m.sender].exp += 50000000
+
+    m.reply('+50000000 XP')
+
+    global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
+
+  } else m.reply('You have claimed daily claims today')
+
+}
+
+handler.help = ['nsiit', 'nsiit']
+
+handler.tags = ['xp']
+
+handler.command = /^(nsiit|nsiit)$/i
+
+handler.owner = false
+
+handler.mods = false
+
+handler.premium = false
+
+handler.group = false
+
+handler.private = false
+
+handler.admin = false
+
+handler.botAdmin = false
+
+handler.fail = null
+
+handler.exp = 0
+
+module.exports = handler
