90eb54f59e063416ae7714a61b256ec708b830a5
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 13:12:40 +0100

    Update twitter.js

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args }) => {
   throw // Fitur belum jadi :)
-  if (!args[0]) throw 'Uhm...url nya mana?'
+  if (!args[0]) throw 'Uhm... where is the URL?'
   global.API('xteam', '/dl/twitter', {
     url: args[0]
   }, 'APIKEY')
