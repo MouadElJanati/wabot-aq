6cee5235d4d615fab64f7909bb1c5aa26f8ec25c
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 04:47:21 +0100

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -2,27 +2,27 @@ const { createHash } = require('crypto')
 let Reg = /(.*)([.|])([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.DATABASE._data.users[m.sender]
-  if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `Foemat salah\n*${usedPrefix}daftar <nama>.umur>*`
+  if (user.registered === true) throw `You have already registered\nWant to re-register? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `Incorrect format\n*${usedPrefix}register <name>.age>*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
-  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  if (!name) throw 'Name cannot be empty (Alphanumeric)'
+  if (!age) throw 'Age cannot be blank (Number)'
   user.name = name
   user.age = parseInt(age)
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-Daftar berhasil!
+Register is successful!
 
 ╭─「 Info 」
-│ Nama: ${name}
-│ Umur: ${age}thn
+│ Name: ${name}
+│ Age: ${age}thn
 │ SN: ${sn}
 ╰────
 `.trim())
 }
-handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
+handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <name>.<age>')
 handler.tags = ['exp']
 
 handler.command = /^(daftar|reg(ister)?)$/i
