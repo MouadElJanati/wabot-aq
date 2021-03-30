743b1e7b8887e317a128972f6c4265d515073dbf
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 05:01:28 +0100

    Update translate.js

--- plugins/translate.js
+++ plugins/translate.js
@@ -1,13 +1,13 @@
 // ariffb - http:/wa.me/6283128734012
 const translate = require('translate-google-api')
 let handler = async (m, { text, usedPrefix }) => {
-    goblok = `contoh: \n${usedPrefix}tr kode bahasa|teks\n${usedPrefix}tr id|thankyou\n\nBahasa yang didukung: https://cloud.google.com/translate/docs/language`
+    goblok = `example: \n${usedPrefix}tr language code|text\n${usedPrefix}tr id|thank you\n\nsupported languages: https://cloud.google.com/translate/docs/language`
     if (!text) throw goblok
 
     let [to, trans] = text.split`|`
 
-    if (!to) throw `Silahkan masukan kode bahasa\ncontoh: \n\n${usedPrefix}tr id|thankyou\n\nBahasa yang didukung: https://cloud.google.com/translate/docs/language`
-    if (!trans) throw `Silahkan masukan kalimat yang ingin diterjemahkan\ncontoh: \n\n${usedPrefix}tr id|thankyou`
+    if (!to) throw `Please enter the language code\nexample: \n\n${usedPrefix}tr id|thank you\n\nsupported languages: https://cloud.google.com/translate/docs/language`
+    if (!trans) throw `Please enter the sentence you want to translate\nexample: \n\n${usedPrefix}tr id|thank you`
 
     try {
         const result = await translate(`${trans}`, {
