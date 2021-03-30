1db0456d8eced95bd878c90b3d8bf47af416506f
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 13:19:51 +0100

    Update unregister.js

--- plugins/unregister.js
+++ plugins/unregister.js
@@ -1,11 +1,11 @@
 const { createHash } = require('crypto')
 let handler = async function (m, { args }) {
-  if (!args[0]) throw 'Serial Number kosong'
+  if (!args[0]) throw 'Serial Number is blank'
   let user = global.DATABASE._data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw 'Serial Number salah'
+  if (args[0] !== sn) throw 'Incorrect Serial Number'
   user.registered = false
-  m.reply(`Unreg berhasil!`)
+  m.reply(`Unregistred!`)
 }
 handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <SN|SERIAL NUMBER>')
 handler.tags = ['exp']
