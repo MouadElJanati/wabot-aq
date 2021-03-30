2e90654b00ae96e2cd1664a0c3790aa350328d0b
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 10:11:27 +0100

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -24,13 +24,13 @@ let handler = async (m, { conn, args }) => {
   let unknown = '_Unknown_'
   let none = '_None_'
   let caption = `
-Konten${isFamilyFriendly ? ' ' : ' *Tidak* '}Family Friendly
-Post oleh ${name} (${authorname || ''}) (${authorlink || ''})
-Diposting pada ${new Date(uploadDate).toLocaleDateString('id', dateConfig)}
+Content ${isFamilyFriendly ? ' ' : ' *Tidak* '}Family Friendly
+Post by ${name} (${authorname || ''}) (${authorlink || ''})
+Posted on ${new Date(uploadDate).toLocaleDateString('id', dateConfig)}
 Size: ${contentSize || unknown}
-Durasi: ${clockString(+ new Date(duration))}
+Duration: ${clockString(+ new Date(duration))}
 Genre: ${genre || none}
-Kualitas: ${quality ? quality : unknown}
+Quality: ${quality ? quality : unknown}
 
 ${description}
 
