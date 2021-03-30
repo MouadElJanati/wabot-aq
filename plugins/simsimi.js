b4ce4b45379579fe03aa1642423e99ecc071244b
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 12:34:38 +0100

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -2,10 +2,10 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   let res = await fetch(global.API('xteam', '/simsimi', { kata: text }, 'APIKEY'))
   let json = await res.json()
-  if (json.status) m.reply(json.jawaban)
+  if (json.status) m.reply(json.answer)
   else throw json
 }
-handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
+handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <text>')
 handler.tags = ['fun']
 handler.command = /^((sim)?simi|simih)$/i
 
