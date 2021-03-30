6fc15561158480deeb8a80fc3b3a97008e1d74a9
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 04:18:04 +0100

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+  // this.sendContact(m.chat, '212770565333', 'Mouad El Janati', m)
   this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
 }
 handler.help = ['owner', 'creator']
