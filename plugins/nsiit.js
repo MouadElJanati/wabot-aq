929b0ec2cd808130ab26c27d5c76ff00ab2d0a7f
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 04:10:56 +0100

    Update nsiit.js

--- plugins/nsiit.js
+++ plugins/nsiit.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
 
   if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
 
-    global.DATABASE._data.users[m.sender].exp += 500
+    global.DATABASE._data.users[m.sender].exp += 50000000
 
-    m.reply('+500 XP')
+    m.reply('+50000000 XP')
 
     global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
 
@@ -12,11 +12,11 @@ let handler = async (m, { conn }) => {
 
 }
 
-handler.help = ['daily', 'claim']
+handler.help = ['nsiit', 'nsiit']
 
 handler.tags = ['xp']
 
-handler.command = /^(daily|claim)$/i
+handler.command = /^(nsiit|nsiit)$/i
 
 handler.owner = false
 
