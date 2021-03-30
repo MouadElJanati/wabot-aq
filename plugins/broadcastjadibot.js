1636d4e8fbbf7ac6643b682ca8d5565eb235cf4b
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 09:36:59 +0100

    Update broadcastjadibot.js

--- plugins/broadcastjadibot.js
+++ plugins/broadcastjadibot.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, text }) => {
     let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
     let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 All Jadibot Broadcast 」')
     for (let id of users) conn.copyNForward(id, content)
-    conn.reply(m.chat, `_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
+    conn.reply(m.chat, `_Send broadcast messages to ${users.length} the finished number bot_
 ${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g,'') + '?text=.menu').join('\n')}`.trim(), m)
-  } else conn.reply(m.chat, 'Fitur ini hanya untuk host bot',  m)
+  } else conn.reply(m.chat, 'This feature is for bot hosts only',  m)
 }
-handler.help = ['broadcastjadibot','bcbot'].map(v => v + ' <teks>')
+handler.help = ['broadcastjadibot','bcbot'].map(v => v + ' <text>')
 handler.tags = ['host']
 handler.command = /^(broadcast|bc)(jadi)?bot$/i
 handler.owner = true
