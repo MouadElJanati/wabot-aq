3b42e29017de0565a85821c3961b48b0ad29a672
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-03-30 12:37:45 +0100

    Update smule.js

--- plugins/smule.js
+++ plugins/smule.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...url nya mana?'
+  if (!args[0]) throw 'Uhm ... where is the url?'
   global.API('xteam', '/dl/smule', {
     url: args[0]
   }, 'APIKEY')
